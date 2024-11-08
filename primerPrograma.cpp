e4dcc18e34ca711de691d8cc415a6c07ea72ab30
Juan <jdgaral27@gmail.com>
2024-11-08 16:48:05 -0400

    bug: error con la funcion por problema de nombres

--- primerPrograma.cpp
+++ primerPrograma.cpp
@@ -4,42 +4,39 @@ using namespace std;
 struct punto {
     int x;
     int y;
-}p;
+};
 
-
-void determinarCuadrante(const punto &pt) {
-    if (pt.x > 0 && pt.y > 0) {
-        cout << "La coordenada (" << pt.x << ", " << pt.y << ") esta en el Cuadrante 1." << endl;
-    } else if (pt.x < 0 && pt.y > 0) {
-        cout << "La coordenada (" << pt.x << ", " << pt.y << ") esta en el Cuadrante 2." << endl;
-    } else if (pt.x < 0 && pt.y < 0) {
-        cout << "La coordenada (" << pt.x << ", " << pt.y << ") esta en el Cuadrante 3." << endl;
-    } else if (pt.x > 0 && pt.y < 0) {
-        cout << "La coordenada (" << pt.x << ", " << pt.y << ") esta en el Cuadrante 4." << endl;
-    } else if (pt.x == 0 && pt.y != 0) {
-        cout << "La coordenada (" << pt.x << ", " << pt.y << ") esta en el eje Y." << endl;
-    } else if (pt.x != 0 && pt.y == 0) {
-        cout << "La coordenada (" << pt.x << ", " << pt.y << ") esta en el eje X." << endl;
+void cuadrante(const punto &p) {
+    if (p.x > 0 && p.y > 0) {
+        cout << "La coordenada (" << p.x << ", " << p.y << ") se encuentra en el 1er cuadrante." << endl;
+    } else if (p.x < 0 && p.y > 0) {
+        cout << "La coordenada (" << p.x << ", " << p.y << ") se encuentra en el 2do cuadrante." << endl;
+    } else if (p.x < 0 && p.y < 0) {
+        cout << "La coordenada (" << p.x << ", " << p.y << ") se encuentra en el 3er cuadrante." << endl;
+    } else if (p.x > 0 && p.y < 0) {
+        cout << "La coordenada (" << p.x << ", " << p.y << ") se encuentra en el 4to cuadrante." << endl;
+    } else if (p.x == 0 && p.y != 0) {
+        cout << "La coordenada (" << p.x << ", " << p.y << ") se encuentra en el eje Y." << endl;
+    } else if (p.x != 0 && p.y == 0) {
+        cout << "La coordenada (" << p.x << ", " << p.y << ") se encuentra en el eje X." << endl;
     } else {
-        cout << "La coordenada (" << pt.x << ", " << pt.y << ") esta en el origen." << endl;
+        cout << "La coordenada (" << p.x << ", " << p.y << ") se encuentra en el origen." << endl;
     }
 }
 
 int main() {
-    punto p;  
+    punto p;  // Definimos p aquí para evitar el conflicto con la declaración global
     char opcion;
 
     do {
-        
-        cout << "Ingrese la p X: ";
+        cout << "Ingrese la coordenada X: ";
         cin >> p.x;
-        cout << "Ingrese la p Y: ";
+        cout << "Ingrese la coordenada Y: ";
         cin >> p.y;
 
-       
-        determinarCuadrante(p);
+        cuadrante(p);
 
-        cout << "Desea ingresar otra p? (s/n): ";
+        cout << "Desea ingresar otra coordenada? (s/n): ";
         cin >> opcion;
 
     } while (opcion == 's' || opcion == 'S');
